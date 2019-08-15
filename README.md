a2e4f228126d46cf65bdb996f9a4f701a2f700b7
kartsridhar <contactkarthik34@gmail.com>
2019-08-15 11:51:21 +0100

    need to deploy

---
+++ README.md
@@ -0,0 +1,20 @@
+# Pynenka
+
+Pynenka is a simple low level twitter bot that replies with the football score of nearest/current game
+
+The live scores are scraped from [livescores.com](https://www.livescores.com/).
+
+The twitter app keys are removed due to security reasons.
+
+To test pynenka simply open twitter and [@pynenka](https://twitter.com/pynenka) followed by the hashtag #Golazo and never miss out on any moment!
+
+### Demo
+![](pynenka-demo.gif)
+
+### TODO
+1. Add Goalscorers and minutes and make it more responsive.
+2. Like and Comment on selected #Football posts
+3. Return live score of requested games
+
+# NOT DEPLOYED YET
+Please clone the repository to test the bot. 
---
+++ bot.py
@@ -0,0 +1,81 @@
+import tweepy
+import time
+import requests
+import os
+from bs4 import BeautifulSoup
+
+API_KEY = ""
+API_SECRET_KEY = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+# Placeholder id = 1060682754885648389
+
+FILE_NAME = 'seen_ids.txt'
+
+def get_last_seen_id(filename):
+    file_read = open(filename, 'r')
+    last_seen = int(file_read.read().strip())
+    file_read.close()
+    return last_seen
+
+def store_last_seen_id(last_seen, filename):
+    file_write = open(filename, 'w')
+    file_write.write(str(last_seen))
+    file_write.close()
+    return
+
+def reply():
+    print("Replying...", flush=True)
+    lastSeenId = get_last_seen_id(FILE_NAME)
+
+    # Get the list of recent mentions
+    mentions = api.mentions_timeline(lastSeenId, tweet_mode='extended')
+    print("Received mentions...", flush=True)
+    for m in reversed(mentions):
+        print("Inspecting the mention...", flush=True)
+        lastSeenId = m.id
+        store_last_seen_id(lastSeenId, FILE_NAME)
+        if '#golazo' in m.full_text.lower():
+            print("found mention", flush=True)
+            scrape_url = "https://www.livescores.com/"
+            print("Scraping match details...")
+
+            try:
+                livescores = requests.get(scrape_url)
+            except Exception as e:
+                print(f'Error occured as {e}')
+            
+            parsed = BeautifulSoup(livescores.text, 'html.parser')
+
+            scores = []
+            matches = []
+
+            for element in parsed.find_all("div", "row-gray"):
+                home = ' '.join(element.find("div", "tright").get_text().strip().split(" "))
+                away = ' '.join(element.find(attrs = {"class": "ply name"}).get_text().strip().split(" "))
+
+                home_score = element.find("div", "sco").get_text().split("-")[0].strip()
+                away_score = element.find("div", "sco").get_text().split("-")[1].strip()
+
+                matches.append(f'{home} vs {away}')
+                scores.append(f'{home_score} - {away_score}')
+                game1 = matches[0] + '\n' + "Score: " + scores[0] + '\n' + '\n'
+                # game2 = matches[1] + '\n' + "Score: " + scores[1] + '\n' + '\n'
+                # game3 = matches[2] + '\n' + "Score: " + scores[2] + '\n'
+
+                tweet = game1 + "#PynenkaLiveScores #Football #TheBeautifulGame"
+            api.update_status('@' + m.user.screen_name + " " + tweet, m.id)
+        print('replied')
+    print('exit mentions')
+
+while True:
+    reply()
+    time.sleep(30)      
+
+
